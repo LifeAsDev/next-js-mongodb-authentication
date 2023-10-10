e4ff31cb9270e21197560a5430fa25bbe1bd48ef
LifeAsDev <angelocraft551@gmail.com>
2023-10-09 20:27:38 -0400

    ui upgrade and delete button fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,15 +17,28 @@ export default function Home() {
   const [selectedImage, setSelectedImage] = useState();
 
   // This function will be triggered when the file field change
-  const imageChange = (e: { target: { files: string | any[] } }) => {
+  const imageChange = (e: any) => {
     if (e.target.files && e.target.files.length > 0) {
       setSelectedImage(e.target.files[0]);
+    } else {
+      console.log("NOOOOOOOOOOOOOO");
+    }
+  };
+
+  const remove_selected = (id_input: any) => {
+    let fileInput = document.getElementById(id_input);
+
+    if (fileInput instanceof HTMLInputElement) {
+      fileInput.value = "";
+      fileInput.dispatchEvent(new Event("change"));
     }
   };
 
   // This function will be triggered when the "Remove This Image" button is clicked
   const removeSelectedImage = () => {
-    setSelectedImage();
+    remove_selected("file");
+
+    setSelectedImage(undefined);
   };
   const numberHandler = (e: any) => {
     const validatedValue = e.target.value.replace(/[^0-9]/g, "");
@@ -60,6 +73,7 @@ export default function Home() {
   };
   const { data: session } = useSession();
   const router = useRouter();
+
   useEffect(() => {
     if (!session) {
       router.push("/");
@@ -69,7 +83,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-4 gap-4">
       <h2 className="text-[2rem]	font-extrabold	">DASHBOARD</h2>
       {session ? (
-        <div className="border w-full  md:w-[40%]  flex border-gray-300 items-center rounded-lg p-4 flex-col justify-center gap-4  mt-8 mg:mt-10">
+        <div className="border w-full  md:w-[500px]  flex border-gray-300 items-center rounded-lg p-4 flex-col justify-center gap-4  mt-8 mg:mt-10">
           <div className="w-full pb-3 flex flex-row	gap-4 items-center 	border-b-[1px] border-gray-300 w-full">
             <UserIcon />
             <input
@@ -93,42 +107,39 @@ export default function Home() {
             className="flex flex-col w-full h-[350px]"
             onSubmit={handleUpload}
           >
-            <label
-              className={`flex flex-col justify-center items-center border-dotted border-2 w-full h-[300px] ${
-                selectedImage ? "" : "cursor-pointer"
-              }`}
-              htmlFor={selectedImage ? "" : "file"}
-            >
-              {selectedImage ? (
-                <>
-                  <div className="relative h-full">
-                    <img
-                      className="w-full h-full object-cover"
-                      src={URL.createObjectURL(selectedImage)}
-                      alt="Thumb"
-                    />
+            {selectedImage ? (
+              <div className="flex flex-col justify-center items-center border-dotted border-2 w-full h-[300px] ">
+                <div className="relative h-full">
+                  <img
+                    className="w-full h-full object-cover"
+                    src={URL.createObjectURL(selectedImage)}
+                    alt="Thumb"
+                  />
+                </div>
+                <div className="relative w-full">
+                  <div
+                    onClick={removeSelectedImage}
+                    className="cursor-pointer p-[.5rem] rounded-[50%] absolute flex justify-center items-center right-[15px] bottom-[15px] w-[3rem] h-[3rem] bg-red-500"
+                  >
+                    <DeleteIcon />
                   </div>
-                  <div className="relative w-full">
-                    <div
-                      onClick={removeSelectedImage}
-                      className="p-[.5rem] rounded-[50%] absolute flex justify-center items-center right-[5px] top-[-53px] w-[3rem] h-[3rem] bg-red-500"
-                    >
-                      <DeleteIcon />
-                    </div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <UploadIcon />
-                  <p className="font-bold	text-gray-500">Click here to upload</p>
-                  <p className="font-[500]		 text-gray-500 text-[.9rem]">
-                    PNG, JPG or GIF(MAX. 800x400px)
-                  </p>
-                </>
-              )}
-            </label>
+                </div>
+              </div>
+            ) : (
+              <label
+                className={`flex flex-col justify-center items-center border-dotted border-2 w-full h-[300px] ${
+                  selectedImage ? "" : "cursor-pointer"
+                }`}
+                htmlFor="file"
+              >
+                <UploadIcon />
+                <p className="font-bold	text-gray-500">Click here to upload</p>
+                <p className="font-[500]		 text-gray-500 text-[.9rem]">
+                  PNG, JPG or GIF(MAX. 800x400px)
+                </p>
+              </label>
+            )}
             <input
-              disabled={selectedImage}
               onChange={imageChange}
               id="file"
               accept="image/*"
